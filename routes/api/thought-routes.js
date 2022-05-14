5c17a5359d393d062539985727e3e84cbdcd6035
lyndsclev <lyndsclev@gmail.com>
2022-05-14 12:50:33 -0400

    thought routes working

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,24 @@
+const router = require('express').Router(); 
+
+const {
+    getAllThoughts, 
+    getThoughtById, 
+    createThought,
+    updateThought,
+    deleteThought
+} = require('../../controllers/thought-controller'); 
+
+// GET and POST at /api/thoughts
+router
+.route('/')
+.get(getAllThoughts)
+.post(createThought); 
+
+// GET one, PUT, and DELETE at /api/thoughts/:id
+router
+.route('/:id')
+.get(getThoughtById)
+.put(updateThought)
+.delete(deleteThought); 
+
+module.exports = router; 
